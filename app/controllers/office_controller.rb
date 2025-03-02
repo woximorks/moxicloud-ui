00709f5b5e3c3b42e0ce6abbc2890c9100390e95
woximorks <aaron.wood@moxiworks.com>
2025-03-02 07:57:26 -0800

    1.5.4

--- app/controllers/office_controller.rb
+++ app/controllers/office_controller.rb
@@ -1,18 +1,25 @@
 class OfficeController < ApplicationController
   def index
-    @associated_attrs = AssociatedAttr.all
+    # Step 1: Get only attributes associated with Office
+    @associated_attrs = AssociatedAttr.select do |attr|
+      attr.associated_endpoints.include?("Office")
+    end
 
-    # Collect all unique product keys specifically for Office
+    # Step 2: Collect all unique product keys specifically for Office
     @product_keys = @associated_attrs.flat_map { |attr| attr.product_info['Office']&.keys }.compact.uniq
 
-    # If the user selected products, filter the associated attributes based on Office products only
+    # Step 3: Filter by selected products
     if params[:products].present?
       selected_products = params[:products]
-      
-      # Select attributes where any of the selected products exist under Office
       @associated_attrs = @associated_attrs.select do |attr|
         attr.product_info['Office']&.keys&.any? { |product| selected_products.include?(product) }
       end
+    end
+
+    # Step 4: Search functionality (only within attributes already associated with Office)
+    if params[:search].present?
+      search_term = params[:search].downcase
+      @associated_attrs = @associated_attrs.select { |attr| attr.attr_title.downcase.include?(search_term) }
     end
   end
 end
--- app/controllers/webuser_controller.rb
+++ app/controllers/webuser_controller.rb
@@ -1,15 +1,25 @@
 class WebuserController < ApplicationController
   def index
-    @associated_attrs = AssociatedAttr.all
+    # Step 1: Get only attributes associated with WebUser
+    @associated_attrs = AssociatedAttr.select do |attr|
+      attr.associated_endpoints.include?("WebUser")
+    end
 
+    # Step 2: Collect all unique product keys specifically for WebUser
     @product_keys = @associated_attrs.flat_map { |attr| attr.product_info['WebUser']&.keys }.compact.uniq
 
+    # Step 3: Filter by selected products
     if params[:products].present?
       selected_products = params[:products]
-      
       @associated_attrs = @associated_attrs.select do |attr|
         attr.product_info['WebUser']&.keys&.any? { |product| selected_products.include?(product) }
       end
+    end
+
+    # Step 4: Search functionality (only within attributes already associated with WebUser)
+    if params[:search].present?
+      search_term = params[:search].downcase
+      @associated_attrs = @associated_attrs.select { |attr| attr.attr_title.downcase.include?(search_term) }
     end
   end
 end
